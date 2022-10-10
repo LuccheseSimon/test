67c2164c4fb5f2917609a6523f8f94e469c7a9b9
Simon <simon.lucchese@nrb.be>
2022-10-10 10:30:43 +0000

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/hello')
+  @Get()
   getHello(): string {
     let hello = this.appService.getHello();
     console.log(hello);
